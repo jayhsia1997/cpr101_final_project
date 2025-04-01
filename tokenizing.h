19715451ecd4d4a1f920e07aa0171a540787208b
Jay Hsia <jayhsia1997@gmail.com>
2025-03-31 20:13:39 -0400

    feat(main): Add tokenizing module with initial implementation and CMake support

---
+++ tokenizing.h
@@ -0,0 +1,18 @@
+/***************************************************************
+
+File: tokenizing.h
+    Author: Jay Hsia
+    Student Number: 112935242
+
+TOKENIZING MODULE HEADER
+***************************************************************/
+
+#ifndef _TOKENIZING_H_
+#define _TOKENIZING_H_
+#include <stdio.h>
+#include <string.h>
+void tokenizing (void) ;
+void tokenizingV1 (void) ;
+void tokenizingV2 (void) ;
+void tokenizingV3 (void) ;
+#endif
---
+++ tokenizingMain.c
@@ -0,0 +1,16 @@
+/***************************************************************
+
+File: tokenizingMain.c
+    Author: Jay Hsia
+    Student Number: 112935242
+
+***************************************************************/
+
+#include <tokenizing.h>
+
+int main(void) {
+    // Test the tokenizing function
+    tokenizing();
+
+    return 0;
+}
