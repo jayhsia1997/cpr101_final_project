5f404aed8126836d070bf6d0012ad5b4127eeeee
loveyouevenwhenimangry <bufferyy@gmail.com>
2025-04-09 20:48:28 -0400

    fundamentals version 3

--- fundamentals.c
+++ fundamentals.c
@@ -3,17 +3,8 @@
 #define NUM_INPUTS_SIZE 10 // Define a constant for the maximum length of numeric input strings.
 #include "fundamentals.h" // Include the "fundamentals.h" header file
 
-void fundamentals() {
-	/* Version 1 */
-	fundamentalsV1();
-	/* Version 2 */
-	fundamentalsV2();
-	/* Version 3
-	fundamentalsV3(); */
-}
-
 /* Version 1 */
-void fundamentalsV1(void) {
+void fundamentals(void) {
 	printf("*** Start of Indexing Strings Demo ***\n"); // Print the start of the program
 
 	char buffer1[BUFFER_SIZE]; // Declare a character array to hold string input
@@ -35,7 +26,6 @@ void fundamentalsV1(void) {
 			if (position >= strlen(buffer1)) { // Check if the position is greater than or equal to the length of the string
 				position = strlen(buffer1) - 1; // If the position is out of bounds, set it to the last valid position
 				printf("Too big... Position reduce to max. available\n"); // Inform the user that the position is out of bounds
-                printf("The character found at %d position is \'%c\'\n", (int)position, buffer1[position]);
 			}
 			printf("The character found at %d position is \'%c\'\n", position, buffer1[position]); // Print the character at the specified position
 		}
@@ -62,5 +52,22 @@ void fundamentalsV2(void){
 	printf("*** End of Measuring Strings Demo ***\n\n"); // Print the end of the program
 }
 
+/* Version 3 */
+void fundamentalsV3(void){
+    printf("*** Start of Copying Strings Demo ***\n"); // Print the start of the program
+    char destination[BUFFER_SIZE]; // Declare a character array 'destination' to hold the string input
+    char source[BUFFER_SIZE]; // Declare a character array 'source' to hold the input string to be copied
+    do{
+        destination[0] = '\0'; // Reset the destination string to empty
+        printf("Destination string is reset to empty\n"); // Inform the user that the destination string has been reset
+        printf("Type the source string (q - to quit): \n"); // Prompt the user to enter a source string, or 'q' to quit
+        fgets(source, BUFFER_SIZE, stdin); // Read the string from the user
+        source[strlen(source) - 1] = '\0'; // Remove the newline character from the source string
 
-	
+        if(strcmp(source, "q") != 0){ // If the input is not "q", copy the source string to the destination
+            strcpy(destination, source); // Copy the source string into the destination array
+            printf("New destination string is \'%s\'\n", destination); // Print the destination string after the copy.
+        }
+    } while (strcmp(source, "q") != 0); // Continue the loop until the user enters 'q' to quit
+    printf("*** End of Copying Strings Demo ***\n\n"); // Print the end of the program
+}
--- fundamentalsMain.c
+++ fundamentalsMain.c
@@ -2,7 +2,9 @@
 #include "fundamentals.c"
 
 int main(void) {
-    //fundamentals(); // Call the fundamentals function
+    
+    fundamentals(); // Call the fundamentals function
     fundamentalsV2(); // Call the fundamentalsV2 function
+    fundamentalsV3(); // Call the fundamentalsV3 function
     return 0;
 }
